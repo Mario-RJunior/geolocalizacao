5d01f969f35733d399dbdff658e8e09e590682b4
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-17 10:01:58 -0300

    Atualizando main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from geolocalizador import *
-from pprint import pprint
 
 if __name__ == '__main__':
     bd = acessa_bd()
     df = gera_dataframe(bd)
-    perg = str(input('Digite a localização (Bairro, Cidade - UF): '))
-    serie = df['endereco_completo'].apply(converte_endereco)
-    df['lat-long'] = serie
-    dist = calcula_distancia_ordenada(perg, df)
-    dist_rel = distancias_relativas(dist)
-    pprint(dist_rel)
-    # km_percorridos = calculo_distancia(dist_rel)
-    # print(km_percorridos)
+    perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
+    perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
+    agrupa_visitas(perg_equipes)
+    calcula_rota(df, perg_local)
