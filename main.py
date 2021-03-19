41f3f47342f0aa8d60a119aaa7a3f8c0b4707468
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-19 17:44:43 -0300

    Adicionando pergunta para data

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     df = gera_dataframe(bd)
     perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
     perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
+    perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
     df = agrupa_visitas(perg_equipes, df)
     map_plot(df)
     #calcula_rota(df, perg_local)
