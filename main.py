2eb8a4aefe607ba4f16af64464afcba1f92e662e
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-20 11:49:07 -0300

    Adicionando a chamada da função formata_data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
     perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
     perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
+    df = formata_data(df, perg_data)
     df = agrupa_visitas(perg_equipes, df)
     map_plot(df)
     #calcula_rota(df, perg_local)
