5e0325ff891f755bbf5ce80ea7c6ef06c7ecdaf6
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-20 19:30:40 -0300

    Reordenando a chamada de funções

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from geolocalizador import *
 
 if __name__ == '__main__':
-    bd = acessa_bd()
-    df = gera_dataframe(bd)
     perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
     perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
     perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
-    df = formata_data(df, perg_data)
+
+    bd = acessa_bd(perg_data)
+    df = gera_dataframe(bd)
     df = agrupa_visitas(perg_equipes, df)
     map_plot(df)
     #calcula_rota(df, perg_local)
