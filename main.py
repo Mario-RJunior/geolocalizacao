ec79330b0c7116196e89e3276a53ff5d13ed80a6
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-25 11:25:08 -0300

    Adicionando tratamento para erro de valores

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from geolocalizador import *
 
 if __name__ == '__main__':
-    perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
-    perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
-    perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
+    try:
 
-    bd = acessa_bd(perg_data)
-    df = gera_dataframe(bd)
-    df = agrupa_visitas(perg_equipes, df)
-    map_plot(df, perg_local)
-    calcula_rota(df, perg_local)
+        perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
+        perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
+        perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
+
+        bd = acessa_bd(perg_data)
+        df = gera_dataframe(bd)
+        df = agrupa_visitas(perg_equipes, df)
+        map_plot(df, perg_local)
+        calcula_rota(df, perg_local)
+
+    except ValueError:
+        print('Valor incorreto. Preencher novamente.')
+
+
