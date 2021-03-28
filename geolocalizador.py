e2f8f2df9ec2681b6ae6ff5f3be676ae6ebe7ac1
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-28 14:43:55 -0300

    Calculando a distância mínima percorrida para cada grupo

--- geolocalizador.py
+++ geolocalizador.py
@@ -97,6 +97,7 @@ def calcula_rota(dataframe, origem):
         df = dataframe.query(f'equipes == {g}')
         enderecos = df['endereco_completo'].to_list()
         dic = {}
+        traj_min = 0
 
         while len(enderecos) > 0:
     
@@ -108,9 +109,12 @@ def calcula_rota(dataframe, origem):
                 dic[end] = distancia
 
             mais_perto = min(dic, key=dic.get)
+            traj_min += dic[mais_perto]
             enderecos.remove(str(mais_perto))
             incio = mais_perto
             pprint(dic)
             dic.clear()
             print(mais_perto)
+        print()
+        print(f'Trajeto mínino: {traj_min}')
         print('-=' * 50)
