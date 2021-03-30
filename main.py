847b1e18db5b209077cae127571df97b03309fa5
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-30 18:03:52 -0300

    Alterando pergunta sobre a localidade inicial

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from geolocalizador import *
 if __name__ == '__main__':
     try:
 
-        perg_local = str(input('Digite a localização (Bairro, Cidade - UF): '))
+        perg_local = str(input('Digite o endereço de partida: '))
         perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
         perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
 
