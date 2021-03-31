54e47473436a8bfdc22301f8bec9110f2c6c9f08
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-31 18:46:16 -0300

    Restaurando o tratamento de exceções de forma geral

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from geolocalizador import *
 
 if __name__ == '__main__':
-
-    perg_local = str(input('Digite o endereço de partida: '))
-    perg_equipes = ''
-
     try:
+
+        perg_local = str(input('Digite o endereço de partida: '))
         perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
-
-    except ValueError:
-        print('Erro ao especificar o número de equipes! Tente novamente.')
-
-    else:
         perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
 
+        bd = acessa_bd(perg_data)
+        df = gera_dataframe(bd)
+        df = agrupa_visitas(perg_equipes, df)
+        map_plot(df, perg_local)
+        calcula_rota(df, perg_local)
 
-    #bd = acessa_bd(perg_data)
-    #df = gera_dataframe(bd)
-    #df = agrupa_visitas(perg_equipes, df)
-    #map_plot(df, perg_local)
-    #calcula_rota(df, perg_local)
+    except ValueError:
+        print('Valor incorreto. Preencher novamente.')
+
+    except AttributeError:
+        print('Valor incorreto. Preencher novamente.')
