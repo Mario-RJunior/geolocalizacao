fb2be0f3973b5374c749450a4f09daae1a869fe8
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-03-31 14:08:56 -0300

    Refazendo tratamento de exceções mais específico para as entradas

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from geolocalizador import *
 
 if __name__ == '__main__':
+    perg_local = str(input('Digite o endereço de partida: '))
+
+    perg_equipes = ''
     try:
-
-        perg_local = str(input('Digite o endereço de partida: '))
         perg_equipes = int(input('Quantas equipes disponíveis para visitas? '))
-        perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
-
-        bd = acessa_bd(perg_data)
-        df = gera_dataframe(bd)
-        df = agrupa_visitas(perg_equipes, df)
-        map_plot(df, perg_local)
-        calcula_rota(df, perg_local)
 
     except ValueError:
-        print('Valor incorreto. Preencher novamente.')
+        print('Erro ao especificar o número de equipes! Tente novamente.')
 
-    except AttributeError:
-        print('Valor incorreto. Preencher novamente.')
+    perg_data = str(input('Insira a data da visita (dd/mm/aaaa): '))
+
+    #bd = acessa_bd(perg_data)
+    #df = gera_dataframe(bd)
+    #df = agrupa_visitas(perg_equipes, df)
+    #map_plot(df, perg_local)
+    #calcula_rota(df, perg_local)
