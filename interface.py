8f845e4115b6ca20a3924faad2d49708e444032f
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-04-02 13:51:11 -0300

    Definindo variáveis para origem, data e número de equipes

--- interface.py
+++ interface.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title('Mapster')
 st.sidebar.title('Menu')
 
-st.sidebar.text_input('Endereço de origem')
-st.sidebar.date_input('Data')
-st.sidebar.selectbox('Número de equipes', [1, 2, 3, 4, 5, 6, 7])
-
+origem = st.sidebar.text_input('Endereço de origem')
+data = st.sidebar.date_input('Data')
+equipes = st.sidebar.selectbox('Número de equipes', [1, 2, 3, 4, 5, 6, 7])
