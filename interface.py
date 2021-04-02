fe5fe3f98c4a06dc630b075a5d1e427942b06850
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-04-02 12:53:51 -0300

    Adicionando campo para informar a quantidade de equipes

--- interface.py
+++ interface.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title('Mapster')
 st.sidebar.title('Menu')
-st.sidebar.text_input('Endereço de origem')
 
+st.sidebar.text_input('Endereço de origem')
 st.sidebar.date_input('Data')
+st.sidebar.selectbox('Número de equipes', [1, 2, 3, 4, 5, 6, 7])
+
