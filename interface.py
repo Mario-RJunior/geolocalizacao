7c8a63dc7a1b36ff9ea76bd9156f5ec1bdc49d72
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-04-02 14:16:26 -0300

    Fazendo teste com a chamada das funções e impressão do dataframe

--- interface.py
+++ interface.py
@@ -7,3 +7,9 @@ st.sidebar.title('Menu')
 origem = st.sidebar.text_input('Endereço de origem')
 data = st.sidebar.date_input('Data')
 equipes = st.sidebar.selectbox('Número de equipes', [1, 2, 3, 4, 5, 6, 7])
+
+if __name__ == '__main__':
+    bd = acessa_bd(data)
+    df = gera_dataframe(bd)
+    df = agrupa_visitas(equipes, df)
+    st.write(df)
