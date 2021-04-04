594ffe3a65d4e75a0d8cc79acdc3801dab8a1133
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-04-03 23:04:45 -0300

    Chamando função filtra_data, reordenando os parâmetros de agrupa_visitas e tratando exceções

--- interface.py
+++ interface.py
@@ -10,8 +10,16 @@ data = st.sidebar.date_input('Data')
 equipes = st.sidebar.selectbox('Número de equipes', [1, 2, 3, 4, 5, 6, 7])
 
 if __name__ == '__main__':
-    bd = acessa_bd(data)
+    bd = acessa_bd()
     df = gera_dataframe(bd)
-    df = agrupa_visitas(equipes, df)
-    m = map_plot(df, origem)
-    folium_static(m)
+    df = filtra_data(df, data)
+    df = agrupa_visitas(df, equipes)
+
+    try:
+        m = map_plot(df, origem)
+
+    except KeyError:
+        st.write('Não há visitas previstas neste dia.')
+
+    else:
+        folium_static(m)
