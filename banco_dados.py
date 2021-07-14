73e18ef59883556e7687a3bc17fd6eb95582a03f
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-07-14 16:26:21 -0300

    Criando classe para o banco de dados

--- banco_dados.py
+++ banco_dados.py
@@ -1,69 +1,69 @@
 import psycopg2
 
 
-def conectar():
-    """
-    Função para conectar ao servidor.
-    """
-    try:
-        conn = psycopg2.connect(
-            database='pacientes',
-            host='localhost',
-            user='teste_db',
-            password='postgres')
+class Bd:
 
-        return conn
+    def conectar(self):
+        """
+        Função para conectar ao servidor.
+        """
+        try:
+            conn = psycopg2.connect(
+                database='pacientes',
+                host='localhost',
+                user='teste_db',
+                password='postgres')
 
-    except psycopg2.Error as e:
-        print(f'Erro na conexão ao MySQL Server: {e}.')
+            return conn
 
+        except psycopg2.Error as e:
+            print(f'Erro na conexão ao MySQL Server: {e}.')
 
-def desconectar(conn):
-    """
-    Função para desconectar do servidor.
-    :param conn: Conexão que se deseja encerrar.
-    """
-    if conn:
-        conn.close()
+    def desconectar(self, conn):
+        """
+        Função para desconectar do servidor.
+        :param conn: Conexão que se deseja encerrar.
+        """
+        if conn:
+            conn.close()
 
+    def listar(self, data):
+        """
+        Função para listar as informações dos pacientes.
+        :param data: Data em que se deseja filtrar os registros do banco de dados.
+        :return: Tupla com as listas de todas as colunas da tabela do banco de dados.
+        """
 
-def listar(data):
-    """
-    Função para listar as informações dos pacientes.
-    :param data: Data em que se deseja filtrar os registros do banco de dados.
-    :return: Tupla com as listas de todas as colunas da tabela do banco de dados.
-    """
+        sql = f"SELECT * FROM visitas WHERE data_visita = '{data}';"
 
-    sql = f"SELECT * FROM visitas WHERE data_visita = '{data}';"
+        conn = self.conectar()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        pacientes = cursor.fetchall()  # Vai pegar o resultado do comando anterior e transformar em uma lista!
 
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    pacientes = cursor.fetchall()  # Vai pegar o resultado do comando anterior e transformar em uma lista!
+        ids = []
+        nomes = []
+        ruas = []
+        numeros = []
+        bairros = []
+        cidades = []
+        estados = []
+        datas = []
 
-    ids = []
-    nomes = []
-    ruas = []
-    numeros = []
-    bairros = []
-    cidades = []
-    estados = []
-    datas = []
+        if len(pacientes) > 0:
 
-    if len(pacientes) > 0:
+            for paciente in pacientes:
+                ids.append(paciente[0])
+                nomes.append(paciente[1])
+                ruas.append(paciente[2])
+                numeros.append(paciente[3])
+                bairros.append(paciente[4])
+                cidades.append(paciente[5])
+                estados.append(paciente[6])
+                datas.append(paciente[7])
 
-        for paciente in pacientes:
-            ids.append(paciente[0])
-            nomes.append(paciente[1])
-            ruas.append(paciente[2])
-            numeros.append(paciente[3])
-            bairros.append(paciente[4])
-            cidades.append(paciente[5])
-            estados.append(paciente[6])
-            datas.append(paciente[7])
+        else:
+            print('Registro não encontrado!')
+        self.desconectar(conn)
 
-    else:
-        print('Registro não encontrado!')
-    desconectar(conn)
-
-    return ids, nomes, ruas, numeros, bairros, cidades, estados, datas
+        return ids, nomes, ruas, numeros, bairros, cidades, estados, datas
